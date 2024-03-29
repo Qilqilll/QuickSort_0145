ebad9974e22a747de6b512a24a2b11216a6b8dde
Qilqilll <aqilf612@gmail.com>
2024-03-29 10:38:53 +0700

    search for an element less than or equal to pivot

--- QuickSort_0145/QuickSort_0145.cpp
+++ QuickSort_0145/QuickSort_0145.cpp
@@ -60,5 +60,13 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
+
+		while ((arr[j] > pivot) && (j > low))
+		{
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j)
 	}
 }
